cc2c123d2d627f89fc042d38ae5aa93a74208875
Hermann2356 <hermannsterling@gmail.com>
2020-09-26 19:35:27 -0400

    Added Master Layout to Vets Index Page. Closes #22

--- pet-clinic-web/src/main/java/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/SfgPetClinicApplication.java
@@ -1,5 +1,8 @@
+import guru.springframework.sfgpetclinic.controllers.VetController;
+import guru.springframework.sfgpetclinic.model.Vet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -9,5 +12,4 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
-
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -49,5 +49,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
+
     }
 }
