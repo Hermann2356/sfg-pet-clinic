0c2d821ae0d49df2a9dc837a4e74094071980181
Hermann2356 <hermannsterling@gmail.com>
2020-09-27 12:50:37 -0400

    Added Visit Entity and Updated Owner Entity with Pet relationship. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /*
  * Created by Hermann Sterling
  * 08/25/2020
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
