6fe2adc8af8611229d677ffb1933d96ba1acfeed
Hermann2356 <hermannsterling@gmail.com>
2020-09-28 23:20:11 -0400

    Fixed Broken Links. Closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -20,4 +20,11 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwner(){
+        return "notimplemented";
+    }
+
+
 }
